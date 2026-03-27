bac9386bc241e3d9b19013958b11e757b3971973
brennschlus <brennschluss@protonmail.com>
2026-03-27 18:53:26 +0300

    removed build artifacts

--- build/image_filters.d
+++
@@ -1,8 +0,0 @@
-image_filters.o: \
- /home/pavel/projects/3ds_photo/source/image/image_filters.c \
- /home/pavel/projects/3ds_photo/source/image/image_filters.h \
- /home/pavel/projects/3ds_photo/source/image/image_buffer.h \
- /opt/devkitpro/libctru/include/3ds/types.h
-/home/pavel/projects/3ds_photo/source/image/image_filters.h:
-/home/pavel/projects/3ds_photo/source/image/image_buffer.h:
-/opt/devkitpro/libctru/include/3ds/types.h:
--- build/music_library.d
+++
@@ -1,3 +0,0 @@
-music_library.o: /home/pavel/projects/3ds_photo/source/fs/music_library.c \
- /home/pavel/projects/3ds_photo/source/fs/music_library.h
-/home/pavel/projects/3ds_photo/source/fs/music_library.h:
